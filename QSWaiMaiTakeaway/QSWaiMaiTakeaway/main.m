65f24c697e817b0b48fdaa8af80e55e1fcae6545
钱烁 <1141331774@qq.com>
2017-07-30 21:07:03 +0800

    01-立项

---
+++ QSWaiMaiTakeaway/QSWaiMaiTakeaway/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QSWaiMaiTakeaway
+//
+//  Created by 钱烁 on 17/7/30.
+//  Copyright © 2017年 钱烁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
