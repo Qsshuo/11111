f7ac0475539d3581f12614976bddb62931895c5a
钱烁 <1141331774@qq.com>
2017-08-01 11:58:46 +0800

    项目准备

---
+++ QSWaiMai/QSWaiMai/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QSWaiMai
+//
+//  Created by 钱烁 on 17/8/1.
+//  Copyright © 2017年 钱烁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
